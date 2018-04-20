05b9ce52929c6e26046a5defc4e287dc770cf3d6
cs5331 <whitefusion@163.com>
2018-04-21 01:17:42 +0800

    scrapy all urls and save in result.json

---
+++ benchmarkSpider/benchmarkSpider/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import json
+from scrapy.exceptions import DropItem
+
+class hackPipeLine(object):
+    def __init__(self):
+        self.file = open('result.json', 'w+')
+        self.seen = set()  
+
+    def process_item(self, item, spider):
+        if item['link'] in self.seen:
+            raise DropItem('Duplicate link %s' % item['link'])
+        self.seen.add(item['link'])
+        line = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(line)
+        return item
