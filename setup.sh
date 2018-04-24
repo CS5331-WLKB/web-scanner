ddc7396794b1addfa02b416769ef9804b46c3185
Liu Chao <lc@smartkarma.com>
2018-04-24 15:47:07 +0800

    update the setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,31 @@
+# copy from local machine to remote machine
+# scp -i privateky -v localfile user@host:/path/to/whereyouwant/thefile
+
+# copy from remote machine to local machine
+# scp user@host:/path/to/remotefile localfile
+
 sudo apt-get update
 
 # install tmux
 sudo apt-get install tmux
+ln -s dotfiles/.tmux.conf ~/.tmux.conf
+ln -s dotfiles/.tmux.conf.local ~/.tmux.conf.local
 
-# update the settings
-ln -s /media/sf_Documents/dotfiles/.tmux.conf .tmux.conf
-ln -s /media/sf_Documents/dotfiles/.tmux.conf.local .tmux.conf.local
+# install vim
+sudo apt -y install vim
+ln -s generate.vm ~/.vimrc
+
+# install other libs
+sudo apt-get install git curl python-pip python-dev build-essential
+
+pip install -r requirements.txt
+
+# install mitmproxy
+sudo apt-get install python-dev
+sudo pip install mitmproxy
+
+# install xsspy
+git clone https://github.com/faizann24/XssPy/
 
 # load tmux settings
-tmux source-file ~/.tmux.conf
-
-# install nvim
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get install neovim
-
-# setup nvim
-sudo apt-get install git exuberant-ctags ncurses-term curl
-
-# install pip
-sudo apt-get install python-pip
-pip install Scrapy
+# tmux source-file ~/.tmux.conf
